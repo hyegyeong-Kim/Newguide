90390b70288a297d17ac4fecf99f65a137f5f89f
jonghwan <subdiary@daum.net>
2023-08-18 14:49:51 +0900

    accordion 수정

--- js/common.js
+++ js/common.js
@@ -71,7 +71,7 @@ function tab_active(_target, evt) {//_target : 대상 / evt : 핸들러
 /* //tab */
 
 /* accordion */
-function accordion(_target, evt){
+function accordion(_target, evt){ // 23.08.18 nextElementSibling 테그가 없는 경우를 위한 수정
     var evt,
     accordion = document.querySelectorAll(_target, evt);
 
@@ -82,27 +82,29 @@ function accordion(_target, evt){
                     const parent_index = Array.from(el.closest('dl').getElementsByTagName('dt')).indexOf(el.parentNode);
 
                     for(j=0; j<el.closest('dl').getElementsByTagName('dt').length; j++){
-                        if(i != j){
+                        if(i != j && el.closest('dl').getElementsByTagName('dt')[j].nextElementSibling != null){
                             el.closest('dl').getElementsByTagName('dt')[j].nextElementSibling.classList.remove('show');
                         }
                     }
                 }
+                
+                if(el.parentNode.nextElementSibling != null){
+                    if(el.parentNode.nextElementSibling.classList.contains('show')){
+                        el.parentNode.nextElementSibling.style.height = '0px'
 
-                if(el.parentNode.nextElementSibling.classList.contains('show')){
-                    el.parentNode.nextElementSibling.style.height = '0px'
+                        el.parentNode.nextElementSibling.addEventListener('transitionend', () => {
+                            el.parentNode.nextElementSibling.classList.remove('show');
+                        }, {once: true});
+                    } else {
+                        el.parentNode.nextElementSibling.classList.add('show');
 
-                    el.parentNode.nextElementSibling.addEventListener('transitionend', () => {
-                        el.parentNode.nextElementSibling.classList.remove('show');
-                    }, {once: true})
-                } else {
-                    el.parentNode.nextElementSibling.classList.add('show');
-
-                    el.parentNode.nextElementSibling.style.height = 'auto'
-                    var height = el.parentNode.nextElementSibling.clientHeight + 'px'
-                    el.parentNode.nextElementSibling.style.height = '0px'
-                    setTimeout(() => {
-                        el.parentNode.nextElementSibling.style.height = height
-                    });
+                        el.parentNode.nextElementSibling.style.height = 'auto'
+                        var height = el.parentNode.nextElementSibling.clientHeight + 'px'
+                        el.parentNode.nextElementSibling.style.height = '0px'
+                        setTimeout(() => {
+                            el.parentNode.nextElementSibling.style.height = height
+                        });
+                    }
                 }
             });
         });
